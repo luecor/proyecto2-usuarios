7f6f4a5ba9666e61ef51511086ecd2260d38f520
Luis Valladolid <eduvalladolid1997@gmail.com>
2023-01-30 17:43:23 -0500

    finished with optional challenge

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 let baseID = 3
-const userBD = [
+let userBD = [
   {
     "id": 1,
     "firstName": "Sahid",
@@ -40,16 +40,55 @@ app.post('/users', (req, res) => {
   res.status(201).json(newUser)
 })
 
-app.get('/users/:id', (req,res) => {
-    const id = Number(req.params.id)
-    const data = userBD.find((user) => id === user.id)
-    if(data){
-        res.status(200).json(data)
-    }else{
-        res.status(404).json({
-            message: 'Invalid ID'
-        })
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const data = userBD.find((user) => id === user.id)
+  if (data) {
+    res.status(200).json(data)
+  } else {
+    res.status(404).json({
+      message: 'Invalid ID'
+    })
+  }
+})
+
+app.delete('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const data = userBD.find((user) => id === user.id)
+  if (data) {
+    userBD = userBD.filter(user => user.id != id)
+    res.status(200).json({
+      message: 'Delete successful'
+    })
+  } else {
+    res.status(404).json({
+      message: 'Invalid ID'
+    })
+  }
+})
+
+app.put('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const validId = userBD.find((user) => id === user.id)
+  const data = req.body
+  if (validId) {
+    const newUser = {
+      id: id,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
+      age: data.age
     }
+    userBD[userBD.findIndex(user => user.id === id)] = newUser
+    res.status(200).json({
+      message: 'successful edit'
+    })
+  } else {
+    res.status(404).json({
+      message: 'Invalid ID'
+    })
+  }
 })
 
 app.listen(9000, () => {
